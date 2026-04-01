a48736ccd02a292d590dc8a39273ee203958b1d5
Papaviron <liga.lol12345@gmail.com>
2026-04-01 20:50:34 +0200

    Step Counter Added Notification with steps Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.medisportuz"
-        minSdk = 34
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
